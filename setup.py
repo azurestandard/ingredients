8a0b861d068386958c36eeaf27c6927790d5e6bb
David Vestal <vestal.d@gmail.com>
2015-03-10 09:50:01 +0100

    Reorganized to a setuptools structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='ingredients',
+    version='0.0.1',
+    author='Jordan Schatz',
+    author_email='info@azurestandard.com',
+    description='Ingredient Parsing Library',
+    license='BSD',
+    url='https://github.com/azurestandard/ingredients',
+    packages=['ingredients'],
+    long_description='Ingredient Parsing Library',
+    classifiers=['Topic :: Utilities']
+)
